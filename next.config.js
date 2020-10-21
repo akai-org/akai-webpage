5fdf57922554e21add9b10e55293f56bf4563583
Krychaxp <krychaxp@op.pl>
2020-10-21 23:10:02 +0200

    fixed next build

--- next.config.js
+++ next.config.js
@@ -2,19 +2,10 @@ const withPWA = require("next-pwa");
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
-  distDir: 'build-node',
-  // reactStrictMode: true,
   devIndicators: {
     autoPrerender: false,
   },
   target: "serverless",
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     require("./scripts/generate-sitemap");
-  //     require("./scripts/build-manifest");
-  //   }
-  //   return config;
-  // },
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
