4b7db64edca3996b84530a584012acfea0015299
Robert Banaszak <robert.banaszak1997@gmail.com>
2020-10-27 20:16:14 +0100

    remove unused files

--- config/index.js
+++ config/index.js
@@ -1,15 +1,22 @@
 module.exports = {
   descr:
-    "Strona internetowa akademickiego koła Aplikacji Internetowych na Politechnice Poznańskiej",
+    "Strona internetowa Akademickiego Koła Aplikacji Internetowych Politechniki Poznańskiej",
   title: "Akademickie Koło Aplikacji Internetowych",
   url: "https://akai.org.pl",
   name: "akai.org.pl",
   img: "/img/img_src.png",
-  keywords: ['PP','PUT','Politechnika','Poznań','Poznańska','AKAI','akademickie','koło','aplikacji','internetowych','JS','React','Next','CSS','SCSS','html',],
-  preconnect: [
-    // "https://www.googletagmanager.com",
-    "https://fonts.googleapis.com",
-    "https://fonts.gstatic.com",
+  keywords: [
+    "PP",
+    "PUT",
+    "Politechnika",
+    "Poznańska",
+    "Poznań",
+    "AKAI",
+    "akademickie",
+    "koło",
+    "aplikacji",
+    "internetowych",
   ],
+  preconnect: ["https://fonts.googleapis.com", "https://fonts.gstatic.com"],
   fonts: ["https://fonts.googleapis.com/css?family=Quicksand:400,600,700"],
 };
