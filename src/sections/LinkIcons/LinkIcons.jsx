0e4bca29878f3e99ac62bac557d9dd374b58fd35
Robert Banaszak <robert.banaszak1997@gmail.com>
2020-12-11 10:18:52 +0100

    update linkIcons

--- src/sections/LinkIcons/LinkIcons.jsx
+++ src/sections/LinkIcons/LinkIcons.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import Section from "../../components/Section";
-import Icon, { blog, apps, calendar } from "../../components/Icon";
+import Icon, {
+  blog,
+  apps,
+  // calendar,
+  facebook,
+  twitter,
+  github,
+} from "../../components/Icon";
 import styles from "./LinkIcons.module.scss";
+
+const socialLinksConfig = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/akai.pp",
+    icon: facebook,
+  },
+  {
+    name: "Github",
+    url: "https://github.com/akai-org",
+    icon: github,
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/akai_pp",
+    icon: twitter,
+  },
+];
 
 const linksConfig = [
   {
@@ -16,26 +41,43 @@ const linksConfig = [
     url: "https://apps.akai.org.pl/",
     icon: apps,
   },
-  {
-    name: "Kalendarz",
-    description: "Obserwuj nasze spotkania i wydarzenia!",
-    url: "#",
-    icon: calendar,
-  },
+  // {
+  //   name: "Kalendarz",
+  //   description: "Obserwuj nasze spotkania i wydarzenia!",
+  //   url: "#",
+  //   icon: calendar,
+  // },
 ];
+
+function LinkIcon({ name, description, url, icon }) {
+  return (
+    <a
+      href={url}
+      className={styles.iconLink}
+      target="_blank"
+      rel="noopener"
+      key={name}
+    >
+      <Icon icon={icon} className={styles.icon} />
+      <span className={styles.text}>{name}</span>
+      {description && <span className={styles.description}>{description}</span>}
+    </a>
+  );
+}
 
 export default function LinkIcons() {
   return (
     <Section className={styles.section}>
-      {linksConfig.map(({ name, description, url, icon }) => (
-        <a href={url} target="_blank" rel="noopener" key={name}>
-          <div className={styles.iconContainer}>
-            <Icon icon={icon} className={styles.icon} />
-            <span className={styles.text}>{name}</span>
-            <span className={styles.description}>{description}</span>
-          </div>
-        </a>
-      ))}
+      <div className={styles.iconsWrapper}>
+        {socialLinksConfig.map((props) => (
+          <LinkIcon {...props} key={props.name} />
+        ))}
+      </div>
+      <div className={styles.iconsWrapper}>
+        {linksConfig.map((props) => (
+          <LinkIcon {...props} key={props.name} />
+        ))}
+      </div>
     </Section>
   );
 }
