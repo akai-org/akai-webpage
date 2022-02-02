46be07863eef74a032eb5ca5a09b57ae1f714413
roertbb <robert.banaszak1997@gmail.com>
2022-02-02 18:32:34 +0000

    Deploying to gh-pages from @ akai-org/akai-webpage@3b132a8288aafc66cdcf3de2964a62ecb3218f2d 🚀

--- _next/static/vd48Jl5MjHirUWKP872dR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-e40d089213815d2a9cba.js","static/css/aa969a8852a62acbd8f7.css","static/chunks/pages/index-ceb0bfcc260d1809c825.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
