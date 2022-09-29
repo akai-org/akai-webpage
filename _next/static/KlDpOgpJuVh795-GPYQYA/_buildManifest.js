f6ba4cad9150c451414e2ac30bcd55fb01701a8e
mlodyoskar <45982530+mlodyoskar@users.noreply.github.com>
2022-09-29 18:31:12 +0000

    Deploying to gh-pages from @ akai-org/akai-webpage@c4978c388cf6195b59b215deac5e6271f5367056 🚀

--- _next/static/KlDpOgpJuVh795-GPYQYA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-97d55f97b8eedd8a.js","static/css/ad721cd3c0732347.css","static/chunks/pages/index-9621855c284a49b4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
